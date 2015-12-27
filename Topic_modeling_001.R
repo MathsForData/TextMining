1c9af907379cda7ad5062516c3e59a357e15ef85
MathsForData <lykke.kroon@gmail.com>
2015-12-27 13:09:04 +0100

    Create Topic_modeling_001.R

---
+++ Topic_modeling_001.R
@@ -0,0 +1,7 @@
+# Instruction to install package tm
+> install.packages("tm")
+
+# Let's see which is the R library paths of our computer
+> .libPaths()
+[1] "C:/Users/A/Documents/R/win-library/3.2"
+[2] "C:/Program Files/R/R-3.2.2/library" 
